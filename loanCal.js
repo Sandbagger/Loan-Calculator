1e112371811fc8f73c94847aae0b88e78b6b003f
Sandbagger <wneal_19@yahoo.co.uk>
2018-03-20 20:42:11 +0000

    Finished prototype

--- loanCal.js
+++ loanCal.js
@@ -11,6 +11,10 @@
  	var month = document.querySelector('#month');
 
  	var apr = document.querySelector('#apr');
+
+ 	var homeSearch = document.querySelector('#home-search');
+ 	var workSearch = document.querySelector('#work-search');
+ 	var employer = document.querySelector('#employer');
  	
 
 //update amount output
@@ -22,6 +26,10 @@
   totalRepayable()
 });
 
+
+borrow.addEventListener('input', function() {
+	amount.value = this.value;
+})
 
 	amount.addEventListener('input', function() {
   console.log(this.value);
@@ -39,7 +47,7 @@ amount.addEventListener('input', function() {
 		apr.value = 26.8;
 	
 	} else {
-		apr.value = 9;
+		apr.value = 10.9;
 	}
 
 	APR = parseFloat(apr.value);
@@ -56,6 +64,23 @@ amount.addEventListener('input', function() {
   totalRepayable()
 });
 
+//set listener for postcode search
+homeSearch.addEventListener('input', function(){
+	var url = "https://www.findyourcreditunion.co.uk/search?Location=" + this.value + "&EmploymentLocation=&EmploymentName=&Associations="
+	document.querySelector('#cta').setAttribute("href", url);
+
+})
+
+//set listener for postcode search
+workSearch.addEventListener('input', function(){
+	var url = "https://www.findyourcreditunion.co.uk/search?Location=&EmploymentLocation="+ this.value +"&EmploymentName=&Associations="
+	document.querySelector('#cta').setAttribute("href", url);
+
+//set listener for postcode search
+employer.addEventListener('input', function(){
+	var url = "https://www.findyourcreditunion.co.uk/search?Location=&EmploymentLocation=&EmploymentName=&Associations="
+	document.querySelector('#cta').setAttribute("href", url);
+
 
 }
 
@@ -64,10 +89,23 @@ var APR = 26.8;
 var PERIOD = 24;
 
 
+
 //Period repayments
 function instalment(){
 
-	var interestPerMonth = APR * (1/1200);
+	var annualRate; 
+
+	if (APR === 10.9) {
+		annualRate = 8.65;
+	} else if (APR === 26.8){
+		annualRate = 24;
+	} else {
+		annualRate = 36;
+	}	
+
+	console.log(annualRate);
+
+	var interestPerMonth = annualRate * (1/1200);
 
 	var emi = (interestPerMonth * AMOUNT) / (1 - (Math.pow((1+interestPerMonth), PERIOD*-1)));
 
@@ -78,6 +116,8 @@ function instalment(){
 	console.log( "Print");
 	console.log(emi);
 	}
+
+
 
 	function setDefaultState() {
   borrow.value = amount.value;
@@ -94,3 +134,5 @@ function totalRepayable(){
 	total = parseFloat(repayment.value) * parseFloat(period.value);
 	totalRepayment.value = total.toFixed(2);
 };
+
+
